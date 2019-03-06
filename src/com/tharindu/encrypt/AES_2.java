9b6e5e62936f533a45b8d1bab40b51d397262b94
aeturnum-tharindu <tharindu.d@aeturnum.com>
2019-03-06 11:22:27 +0530

    Initial commit

---
+++ src/com/tharindu/encrypt/AES_2.java
@@ -0,0 +1,107 @@
+package com.tharindu.encrypt;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+
+public class AES_2 {
+    private static SecretKeySpec secretKey;
+
+    private static byte[] key;
+
+    private static String decryptedString;
+
+    private static String encryptedString;
+
+    public static void setKey(String myKey) {
+
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes("UTF-8");
+            System.out.println(key.length);
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16); // use only first 128 bit
+            System.out.println(key.length);
+            System.out.println(new String(key, "UTF-8"));
+            secretKey = new SecretKeySpec(key, "AES");
+
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String getDecryptedString() {
+        return decryptedString;
+    }
+
+    public static void setDecryptedString(String decryptedString) {
+        AES_2.decryptedString = decryptedString;
+    }
+
+    public static String getEncryptedString() {
+        return encryptedString;
+    }
+
+    public static void setEncryptedString(String encryptedString) {
+        AES_2.encryptedString = encryptedString;
+    }
+
+    public static String encrypt(String strToEncrypt) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+            setEncryptedString(Base64.encodeBase64String(cipher.doFinal(strToEncrypt.getBytes("UTF-8"))));
+
+        } catch (Exception e) {
+
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    public static String decrypt(String strToDecrypt) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            setDecryptedString(new String(cipher.doFinal(Base64.decodeBase64(strToDecrypt))));
+
+        } catch (Exception e) {
+
+            System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    public static void main(String args[]) {
+        final String strToEncrypt = "My text to encrypt";
+        final String strPssword = "PBC";
+        AES_2.setKey(strPssword);
+
+        AES_2.encrypt(strToEncrypt.trim());
+
+        System.out.println("String to Encrypt: " + strToEncrypt);
+        System.out.println("Encrypted: " + AES_2.getEncryptedString());
+
+        final String strToDecrypt = AES_2.getEncryptedString();
+        AES_2.decrypt(strToDecrypt.trim());
+
+        System.out.println("String To Decrypt : " + strToDecrypt);
+        System.out.println("Decrypted : " + AES_2.getDecryptedString());
+
+    }
+}
